f2d242f1589e50ed2350ad3f730d348198e6c451
Tomáš Bosák <tombos255@gmail.com>
2024-07-14 15:38:35 +0200

    Connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
-const express = require('express');
+// Import .env variables
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "127.0.0.1";
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from server!' });
-}
-);
+// SSL
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "10mb" }));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 8000');
-}
-);
+// CORS
+app.use(cors());
+
+// Parse JSON
+app.use(bodyParser.json());
+
+mongoose
+		.connect(process.env.DB_CONNECTION)
+		.then(() => console.log("Connected to MongoDB!"))
+		.catch((error) => console.error("Error connecting to MongoDB", error));
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, HOST, () => {
+		console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
