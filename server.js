e6bc4c02202ffc5bdbffa6cd70ee0aa2d67f28a0
Tomáš Bosák <tombos255@gmail.com>
2024-07-15 23:15:19 +0200

    Data evalutaion

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "127.0.0.1";
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 // SSL
@@ -16,11 +15,25 @@ app.use(express.json({ limit: "10mb" }));
 // CORS
 app.use(cors());
 
-// Parse JSON
-app.use(bodyParser.json());
-
 app.post("/", (req, res) => {
 	res.json({ message: "Hello World!" });
+
+});
+
+function IsNullorEmptyOrUndefined(str) {
+    return str === null || str === undefined || (typeof str === 'string' && str.trim() === "");
+}
+
+
+app.post("/users/register", (req, res) => {
+	// check if each field is not null, empty, or undefined
+	for (const key in req.body) {
+		if (IsNullorEmptyOrUndefined(req.body[key])) {
+			return res.json({ message: `${key} is required` });
+		}
+	}
+
+	return res.json({ message: "All fields are valid!" });
 
 });
 
